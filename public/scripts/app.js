2270faeeff6d9a94fcb3a1d0103a14e304fc5ac4
AtefeFazeli <AtefeFazeli.com>
2023-07-23 13:30:59 +0430

    start mobile shopping cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,10 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
 const submenuToggle = document.querySelector(".submenu--toggle");
 const submenu = document.querySelector(".submenu");
+const navIcon = document.querySelector(".nav-icon");
+const navbar = document.querySelector(".navbar");
+const overlay = document.querySelector(".overlay")
+const navbarCloseBtn = document.querySelector(".navbar-close-btn")
 
 toggleThemeBtns.forEach(btn =>{
     btn.addEventListener("click" , () => {
@@ -19,4 +23,21 @@ toggleThemeBtns.forEach(btn =>{
 submenuToggle.addEventListener("click", (event)=>{
     event.currentTarget.parentElement.classList.toggle("text-orange-300")
     submenu.classList.toggle("submenu--open")
+})
+
+
+navIcon.addEventListener("click",()=>{
+    navbar.classList.remove("-right-64")
+    navbar.classList.add("navbar--open")
+    overlay.classList.add("overlay--visible")
+})
+navbarCloseBtn.addEventListener("click",()=>{
+    navbar.classList.remove("navbar--open")
+    navbar.classList.add("-right-64")
+    overlay.classList.remove("overlay--visible")
+})
+overlay.addEventListener("click",()=>{
+    navbar.classList.remove("navbar--open")
+    navbar.classList.add("-right-64")
+    overlay.classList.remove("overlay--visible")
 })
